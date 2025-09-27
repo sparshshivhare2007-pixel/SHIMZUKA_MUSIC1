25d988221fadc1f2f550e3d272b6d9bc42b1db2b
⏤͟͞ 𝙎𝙋𝘼𝙍𝙎𝙃 𝘽𝘼𝙉𝙄𝙔𝘼 <sparshshivhare2007@gmail.com>
2025-09-27 18:45:46 +0530

    Update config.py

--- config.py
+++ config.py
@@ -15,7 +15,7 @@ BOT_TOKEN = getenv("BOT_TOKEN")
 # -------------------------------------------------------
 OWNER_USERNAME = getenv("OWNER_USERNAME","HYE_BABU")
 # --------------------------------------------------------
-BOT_USERNAME = getenv("BOT_USERNAME","shimzukaXmusic_robot")
+BOT_USERNAME = getenv("BOT_USERNAME","shizuuXmusicbot")
 # --------------------------------------------------------
 BOT_NAME = getenv("BOT_NAME")
 # ---------------------------------------------------------
@@ -30,7 +30,7 @@ DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 17000))
 LOGGER_ID = int(getenv("LOGGER_ID", None))
 
 # Get this value from @PURVI_HELP_BOT on Telegram by /id
-OWNER_ID = int(getenv("OWNER_ID", 8158429107 ))
+OWNER_ID = int(getenv("OWNER_ID", 8158429107))
 
 
 ## Fill these variables if you're deploying on heroku.
